56516564e22d0c6c99bfd128502250ec56dbd9dd
Anus khalil <117922339+Anuskhalil@users.noreply.github.com>
2023-06-26 09:38:33 +0500

    Add files via upload

---
+++ Assignment-02/app.js
@@ -0,0 +1,12 @@
+//Check number is Odd or Even
+
+var CheckNum = prompt("Enter your Number");
+
+if (CheckNum % 2 === 0) {
+
+document.write("CheckNum is Even " , CheckNum);
+
+}
+else if (CheckNum % 2 != 0) {
+    document.write("Number is Odd " , CheckNum);
+}
